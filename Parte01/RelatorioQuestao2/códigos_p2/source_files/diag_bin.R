ad03c3c0ee356fe7fed4a68307bcfbf4c6b6b944
Leonardo Uchoa <leonardouchoa.pedreira@gmail.com>
2017-06-11 15:09:25 -0300

    fkn graphs

--- Parte01/RelatorioQuestao2/códigos_p2/source_files/diag_bin.R
+++ Parte01/RelatorioQuestao2/códigos_p2/source_files/diag_bin.R
@@ -14,8 +14,9 @@ di <- (h/(1-h))*(ts^2)
 a <- max(td)
 b <- min(td)
 par(mfrow=c(2,2))
+par(pty="s")
 plot(td,xlab="Indice", ylab="Residuo Componente do Desvio",
-ylim=c(b-1,a+1), pch=16,cex.axis=1.1,cex.lab=1.1,cex=1.1,cex.axis=1.1,cex.lab=1.1)
+ylim=c(b-1,a+1), pch=16,cex.axis=1.1,cex.lab=1.1,cex=1.1,cex.axis=.5,cex.lab=1.1)
 abline(2,0,lty=2)
 abline(-2,0,lty=2)
 abline(0,0,lty=2)
@@ -24,7 +25,7 @@ abline(0,0,lty=2)
 # title(sub="(c)")
 fited = fitted(fit.model)
 plot(fited ,td,xlab="valor ajustado (média)", ylab="Residuo Componente do Desvio",ylim=c(b-1,a+1), pch=16,
-main="",cex=1.1,cex.axis=1.1,cex.lab=1.1)
+main="",cex=1.1,cex.axis=.5,cex.lab=1.1)
 abline(2,0,lty=2)
 abline(-2,0,lty=2)
 abline(0,0,lty=2)
@@ -34,7 +35,7 @@ abline(0,0,lty=2)
 #
 eta = predict(fit.model)
 z = eta + resid(fit.model, type="pearson")/sqrt(w)
-plot(predict(fit.model),z,xlab="Preditor Linear",ylab="Variavel z", pch=16,main="",cex=1.1,cex.axis=1.1,cex.lab=1.1)
+plot(predict(fit.model),z,xlab="Preditor Linear",ylab="Variavel z", pch=16,main="",cex=1.1,cex.axis=.5,cex.lab=1.1)
 lines(smooth.spline(predict(fit.model), z, df=2))
 #
 #---------------------------------------------------------------#
